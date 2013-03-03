75c375568dae457f977815a3526a21a28d30867d
wyrihaximus <wyrihaximus@xps420.home.wyrihaxim.us>
2013-03-03 21:19:11 +0100

    Added keep alive handler. nginx kills the connection after a x number of secconds of no inactivity, when the clients subscribes to the keepAlive topic every 5 seconds a broadcast (ping) is send out to the clients preventing the connection from beeing dropped. This might not be wished for in every situation so use with care.

---
+++ Event/RatchetKeepAliveListener.php
@@ -0,0 +1,33 @@
+<?php
+
+App::uses('CakeEventListener', 'Event');
+
+class RatchetKeepAliveListener implements CakeEventListener {
+
+    private $loop;
+    private $timer;
+    
+    public function implementedEvents() {
+        return array(
+            'Rachet.WampServer.construct' => 'construct',
+            'Rachet.WampServer.onSubscribeNewTopic.keepAlive' => 'onSubscribeNewTopic',
+            'Rachet.WampServer.onUnSubscribeStaleTopic.keepAlive' => 'onUnSubscribeStaleTopic',
+        );
+    }
+    
+    public function construct($event) {
+        $this->loop = $event->data['loop'];
+    }
+    
+    public function onSubscribeNewTopic($event) {
+        $this->timer = $this->loop->addPeriodicTimer(5, function() use ($event) {
+            $event->data['topic']->broadcast('ping');
+        }, true);
+        
+        $event->data['topic']->broadcast('ping');
+    }
+    
+    public function onUnSubscribeStaleTopic($event) {
+        $this->loop->cancelTimer($this->timer);
+    }
+}
