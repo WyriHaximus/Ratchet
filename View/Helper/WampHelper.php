edb9ea559c1109c5e39a66f0edc94e6b7ff028d7
wyrihaximus <wyrihaximus@xps420.home.wyrihaxim.us>
2013-03-04 22:31:33 +0100

    Moved the wsuri definition to the helper to use the configuration options provided in bootstrap

--- View/Helper/WampHelper.php
+++ View/Helper/WampHelper.php
@@ -7,8 +7,19 @@ class WampHelper extends AppHelper {
     );
     
     public function init() {
-        
-        $this->Html->scriptBlock('WEB_SOCKET_SWF_LOCATION = "' . $this->Html->url('/Ratchet/swf/WebSocketMain.swf', true) . '"', array(
+        $block = '';
+        $block .= 'WEB_SOCKET_SWF_LOCATION = "' . $this->Html->url('/Ratchet/swf/WebSocketMain.swf', true) . '";' . PHP_EOL;
+        $block .= 'var cakeWamp = window.cakeWamp || {};' . PHP_EOL;
+        $block .= 'cakeWamp.options = {';
+        $block .= 'retryDelay: ' . (int) Configure::read('Ratchet.Client.retryDelay') . ',';
+        $block .= 'maxRetries: ' . (int) Configure::read('Ratchet.Client.maxRetries') . '';
+        $block .= '};' . PHP_EOL;
+        $block .= 'var wsuri = "';
+        $block .= ((Configure::read('Ratchet.Connection.external.secure')) ? 'wss' : 'ws') . '://';
+        $block .= Configure::read('Ratchet.Connection.external.hostname');
+        $block .= ':' . Configure::read('Ratchet.Connection.external.port');
+        $block .= '/' . Configure::read('Ratchet.Connection.external.path') . '";';
+        $this->Html->scriptBlock($block, array(
             'inline' => false,
         ));
         $this->Html->script('cache/Ratchet.wamp', array('block' => 'script'));
