8f271156935b09c05e7d0e4069f555638844aa11
wyrihaximus <wyrihaximus@xps420.home.wyrihaxim.us>
2013-03-04 22:27:58 +0100

    Brand new options

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,10 +1,34 @@
 <?php
 
+Configure::write('Ratchet', array(
+    'Client' => array(
+        'retryDelay' => 500, // Not the best option but it speeds up development
+        'maxRetries' => 500, // Keep on trying! (Also not the best option)
+    ),
+    'Connection' => array(
+        'flashPolicy' => array(
+            'address' => '0.0.0.0',
+            'port' => 843,
+        ),
+        'websocket' => array(
+            'address' => '0.0.0.0',
+            'port' => 54321,
+        ),
+        'external' => array(
+            'hostname' => 'localhost',
+            'port' => 80,
+            'path' => 'websocket',
+            'secure' => false,
+        ),
+    ),
+));
+
 App::uses('CakeEventManager', 'Event');
-App::uses('CakeEvent', 'Event');
 
 App::uses('RatchetCallUrlListener', 'Ratchet.Event');
 App::uses('RatchetConnectionStatisticsListener', 'Ratchet.Event');
+App::uses('RatchetKeepAliveListener', 'Ratchet.Event');
 
 CakeEventManager::instance()->attach(new RatchetCallUrlListener());
 CakeEventManager::instance()->attach(new RatchetConnectionStatisticsListener());
+CakeEventManager::instance()->attach(new RatchetKeepAliveListener());
