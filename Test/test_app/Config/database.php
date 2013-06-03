d00a726d2b2ca5099a39d9e195718ca4ffe524bd
Cees-Jan <Cees-Jan@xps8500.home.wyrihaxim.us>
2013-06-03 21:39:00 +0200

    Corrected mysql configuration

--- Test/test_app/Config/database.php
+++ Test/test_app/Config/database.php
@@ -3,8 +3,8 @@ class DATABASE_CONFIG {
     public $test = array(
         'datasource' => 'Database/Mysql',
         'database' => 'cakephp_test',
-        'host' => '0.0.0.0',
-        'login' => 'travis',
+        'host' => '127.0.0.1',
+        'login' => 'root',
         'host' => '',
         'persistent' => false,
     );
