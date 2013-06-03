0de13ec0470f9e1147a10ba3c0729b17dd1526a4
Cees-Jan <Cees-Jan@xps8500.home.wyrihaxim.us>
2013-06-03 21:23:52 +0200

    Moved the database.php creation from the travis file into a file in the test app to try and keep the travis file clean

---
+++ Test/test_app/Config/database.php
@@ -0,0 +1,11 @@
+<?php
+class DATABASE_CONFIG {
+    public $test = array(
+        'datasource' => 'Database/Mysql',
+        'database' => 'cakephp_test',
+        'host' => '0.0.0.0',
+        'login' => 'travis',
+        'host' => '',
+        'persistent' => false,
+    );
+}
