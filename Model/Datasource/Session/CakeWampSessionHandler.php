7a6ba8f5f18e171f93182efa12ad5a8184b840b9
Cees-Jan <Cees-Jan@xps8500.home.wyrihaxim.us>
2013-10-20 15:53:06 +0200

    Updated headers

--- Model/Datasource/Session/CakeWampSessionHandler.php
+++ Model/Datasource/Session/CakeWampSessionHandler.php
@@ -1,13 +1,13 @@
 <?php
 
-/*
-* This file is part of Ratchet for CakePHP.
-*
-** (c) 2012 - 2013 Cees-Jan Kiewiet
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+/**
+ * This file is part of Ratchet for CakePHP.
+ *
+ ** (c) 2012 - 2013 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 class CakeWampSessionHandler implements SessionHandlerInterface {
 
