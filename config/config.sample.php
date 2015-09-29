99e114cbe89977d9287c732d958d355fa2ad27c5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-09-29 21:36:27 +0200

    Added config example

---
+++ config/config.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'WyriHaximus' => [
+        'Ratchet' => [
+            'Connection' => [
+                'Websocket' => [
+                    'adddress' => '0.0.0.0',
+                    'port' => 12345,
+                ],
+            ],
+        ],
+    ],
+];
