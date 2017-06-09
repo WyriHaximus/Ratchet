f9ceefb5a8c71192ca16974ceace812f6cf61c9a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-06-09 16:44:47 +0200

    Authorization checks before message events

---
+++ src/Event/AuthorizeEvent.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * This file is part of Ratchet.
+ *
+ ** (c) 2016 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WyriHaximus\Ratchet\Event;
+
+use Cake\Event\Event;
+use Cake\Event\EventManager;
+use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
+use React\Promise\PromiseInterface;
+use Thruway\Message\ActionMessageInterface;
+use Thruway\Session;
+
+class AuthorizeEvent extends Event
+{
+    const EVENT = 'WyriHaximus.Ratchet.%s.authorize';
+
+    public static function realmEvent($realm)
+    {
+        return sprintf(self::EVENT, $realm);
+    }
+
+    /**
+     * @var Deferred
+     */
+    private $deferred;
+
+    /**
+     * @param LoopInterface $loop
+     * @return static
+     */
+    public static function create($realm, Session $session, ActionMessageInterface $actionMsg)
+    {
+        return new static(self::realmEvent($realm), $actionMsg, [
+            'realm' => $realm,
+            'session' => $session,
+            'actionMessage' => $actionMsg,
+        ]);
+    }
+
+    public function __construct($name, $subject = null, $data = null)
+    {
+        parent::__construct($name, $subject, $data);
+        $this->deferred = new Deferred();
+    }
+
+    /**
+     * @return Session
+     */
+    public function getRealm()
+    {
+        return $this->data()['realm'];
+    }
+
+    /**
+     * @return Session
+     */
+    public function getSession()
+    {
+        return $this->data()['session'];
+    }
+
+    /**
+     * @return ActionMessageInterface
+     */
+    public function getActionMessage()
+    {
+        return $this->data()['actionMessage'];
+    }
+
+    /**
+     * @return PromiseInterface
+     */
+    public function promise()
+    {
+        return $this->deferred->promise();
+    }
+
+    public function reject()
+    {
+        $this->deferred->reject();
+    }
+}
--- src/Security/AuthorizationManager.php
+++ src/Security/AuthorizationManager.php
@@ -11,13 +11,31 @@
 
 namespace WyriHaximus\Ratchet\Security;
 
+use Cake\Event\EventManager;
+use function React\Promise\reject;
 use Thruway\Event\MessageEvent;
 use Thruway\Event\NewRealmEvent;
+use Thruway\Message\ErrorMessage;
 use Thruway\Module\RealmModuleInterface;
 use Thruway\Module\RouterModuleClient;
+use WyriHaximus\Ratchet\Event\AuthorizeEvent;
 
 class AuthorizationManager extends RouterModuleClient implements RealmModuleInterface
 {
+    /**
+     * @var EventManager
+     */
+    private $eventManager;
+
+    /**
+     * @param EventManager $eventManager
+     */
+    public function setEventManager(EventManager $eventManager)
+    {
+        $this->eventManager = $eventManager;
+        return $this;
+    }
+
     /**
      * Listen for Router events.
      * Required to add the authorization module to the realm
@@ -59,10 +77,13 @@ class AuthorizationManager extends RouterModuleClient implements RealmModuleInte
     /**
      * @param MessageEvent $msg
      */
-    public function authorize(MessageEvent $msg)
+    public function authorize(MessageEvent $messageEvent)
     {
-        /**
-         * WIP auth handling
-         */
+        $event = AuthorizeEvent::create($this->getRealm(), $messageEvent->session, $messageEvent->message);
+        $event->promise()->otherwise(function () use ($messageEvent) {
+            $messageEvent->session->sendMessage(ErrorMessage::createErrorMessageFromMessage($messageEvent->message, "wamp.error.not_authorized"));
+            $messageEvent->stopPropagation();
+        });
+        $this->eventManager->dispatch($event);
     }
 }
