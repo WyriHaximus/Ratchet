ebb550d15f409623dcac542ad7e719893adb2cfb
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-02-11 17:45:55 +0100

    authId => authid

--- src/Security/JWTAuthProvider.php
+++ src/Security/JWTAuthProvider.php
@@ -83,7 +83,7 @@ final class JWTAuthProvider extends AbstractAuthProviderClient
         }
 
         if ($token->verify(new Sha256(), $this->activeRealms[$iss])) {
-            return resolve(["SUCCESS", ['authid' => $token->getClaim('authId')]]);
+            return resolve(["SUCCESS", ['authid' => $token->getClaim('authid')]]);
         }
 
         return resolve(["FAILURE"]);
